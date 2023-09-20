4bc897bd9fb6795dfecf141263fc184b36aeb1f3
andreych <cad.dsw.dwa@gmail.com>
2023-09-20 10:52:07 +0500

    init project

---
+++ storage/ViewModels/MainViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace storage.ViewModels;
+
+public class MainViewModel : ViewModelBase
+{
+    public string Greeting => "Welcome to Avalonia!";
+}
---
+++ storage/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace storage.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
