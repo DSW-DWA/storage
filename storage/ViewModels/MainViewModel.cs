7512cca6cb292d7814d0a6d3ab128add91b78c27
andreych <cad.dsw.dwa@gmail.com>
2023-09-27 11:34:46 +0500

    add first version of word report

--- storage/ViewModels/MainViewModel.cs
+++ storage/ViewModels/MainViewModel.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Data;
 using System.IO;
+using System.Linq;
 using System.Threading;
+using Avalonia.Animation.Easings;
 using MsBox.Avalonia;
 using MsBox.Avalonia.Enums;
 using Newtonsoft.Json;
@@ -42,52 +44,62 @@ public class MainViewModel : ReactiveObject
     public void ExportToWord()
     {
         Thread.Sleep(5000);
-        // var dataSet = _dataAccess.DS;
-        // var outputPath = @"./report.docx";
-        // using (var doc = DocX.Create(outputPath))
-        // {
-        //     foreach (DataTable table in dataSet.Tables)
-        //     {
-        //         doc.InsertParagraph(table.TableName).Bold().FontSize(14d).SpacingBefore(10d).SpacingAfter(10d);
-        //
-        //         var wordTable = doc.AddTable(table.Rows.Count + 1, table.Columns.Count);
-        //
-        //         for (var colIndex = 0; colIndex < table.Columns.Count; colIndex++)
-        //             wordTable.Rows[0].Cells[colIndex].Paragraphs[0].Append(table.Columns[colIndex].ColumnName);
-        //
-        //         for (var rowIndex = 0; rowIndex < table.Rows.Count; rowIndex++)
-        //             for (var colIndex = 0; colIndex < table.Columns.Count; colIndex++)
-        //                 wordTable.Rows[rowIndex + 1].Cells[colIndex].Paragraphs[0]
-        //                     .Append(table.Rows[rowIndex][colIndex].ToString());
-        //
-        //         doc.InsertTable(wordTable);
-        //         doc.InsertParagraph();
-        //     }
-        //
-        //     doc.Save();
-        // }
+        var dataset = DataAccess.GetDataSet();
+        var outputpath = @"./report.docx";
+        using (var doc = DocX.Create(outputpath))
+        {
+            doc.InsertParagraph("Материалы").Bold().FontSize(14d).SpacingBefore(10d).SpacingAfter(10d);
+            var MatTable = MaterialAccess.GetAll();
+            var CatTable = CategoryAccess.GetAll();
+            var RecTable = MaterialReceiptAccess.GetAll();
+            var InvTable = InvoiceAccess.GetAll();
+
+            var wordTable = doc.AddTable(MatTable.Count+1, 5);
+
+            wordTable.Rows[0].Cells[0].Paragraphs[0].Append("ID");
+            wordTable.Rows[0].Cells[1].Paragraphs[0].Append("Имя");
+            wordTable.Rows[0].Cells[2].Paragraphs[0].Append("Категория");
+            wordTable.Rows[0].Cells[3].Paragraphs[0].Append("Ед. Измерения");
+            wordTable.Rows[0].Cells[4].Paragraphs[0].Append("Дата поступления на склад");
+            
+            for(var rowIndex = 0; rowIndex < MatTable.Count; rowIndex++)
+            {
+                var recId = RecTable.Where(x => x.MaterialId == MatTable[rowIndex].Id).Select(x => x.Id).ToList();
+
+                wordTable.Rows[rowIndex + 1].Cells[0].Paragraphs[0].Append(MatTable[rowIndex].Id.ToString());
+                wordTable.Rows[rowIndex + 1].Cells[1].Paragraphs[0].Append(MatTable[rowIndex].Name.ToString());
+                wordTable.Rows[rowIndex + 1].Cells[2].Paragraphs[0].Append(CatTable.Where(x => x.Id == MatTable[rowIndex].Id).FirstOrDefault()?.Name);
+                wordTable.Rows[rowIndex + 1].Cells[3].Paragraphs[0].Append(CatTable.Where(x => x.Id == MatTable[rowIndex].Id).FirstOrDefault()?.MeasureUnit);
+                wordTable.Rows[rowIndex + 1].Cells[4].Paragraphs[0].Append(InvTable.Where(x => recId.Contains(x.Id)).OrderBy(x => x.CreatedAt).FirstOrDefault()?.ToString());
+            }
+
+            doc.InsertTable(wordTable);
+            doc.InsertParagraph();
+
+            doc.Save();
+        }
     }
 
     public void ExportToExcel()
     {
         Thread.Sleep(5000);
-        // var dataSet = _dataAccess.DS;
-        // var outputPath = @"./report.xlsx";
-        // ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
-        // using (var package = new ExcelPackage())
-        // {
-        //     foreach (DataTable table in dataSet.Tables)
-        //     {
-        //         var worksheet = package.Workbook.Worksheets.Add(table.TableName);
-        //         for (var colIndex = 0; colIndex < table.Columns.Count; colIndex++)
-        //             worksheet.Cells[1, colIndex + 1].Value = table.Columns[colIndex].ColumnName;
-        //
-        //         for (var rowIndex = 0; rowIndex < table.Rows.Count; rowIndex++)
-        //             for (var colIndex = 0; colIndex < table.Columns.Count; colIndex++)
-        //                 worksheet.Cells[rowIndex + 2, colIndex + 1].Value = table.Rows[rowIndex][colIndex];
-        //     }
-        //
-        //     File.WriteAllBytes(outputPath, package.GetAsByteArray());
-        // }
+        /*var dataset = _dataaccess.ds;
+        var outputpath = @"./report.xlsx";
+        excelpackage.licensecontext = licensecontext.noncommercial;
+        using (var package = new excelpackage())
+        {
+            foreach (datatable table in dataset.tables)
+            {
+                var worksheet = package.workbook.worksheets.add(table.tablename);
+                for (var colindex = 0; colindex < table.columns.count; colindex++)
+                    worksheet.cells[1, colindex + 1].value = table.columns[colindex].columnname;
+
+                for (var rowindex = 0; rowindex < table.rows.count; rowindex++)
+                    for (var colindex = 0; colindex < table.columns.count; colindex++)
+                        worksheet.cells[rowindex + 2, colindex + 1].value = table.rows[rowindex][colindex];
+            }
+
+            file.writeallbytes(outputpath, package.getasbytearray());
+        }*/
     }
 }
